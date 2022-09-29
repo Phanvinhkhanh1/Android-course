48f1c356f3e58da279504ae18489d651dd3d4f52
Phan Vinh Khanh <Khanh161297@gmail.com>
2022-09-29 07:49:22 +0700

    setImageBackGround in Android

--- ImageView/app/src/main/java/com/example/imageview/MainActivity.java
+++ ImageView/app/src/main/java/com/example/imageview/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.imageview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -9,7 +10,7 @@ import android.widget.RelativeLayout;
 public class MainActivity extends AppCompatActivity {
 
     ImageView imgView;
-//    RelativeLayout relativeLayoutScreen;
+    RelativeLayout relativeLayoutScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imgView = (ImageView) findViewById(R.id.imageViewAndroid);
-//        relativeLayoutScreen = findViewById(R.id.screenBackGround);
+        relativeLayoutScreen = findViewById(R.id.screenBackground);
 
+        relativeLayoutScreen.setBackgroundColor(Color.rgb(100, 200, 50));
         imgView.setImageResource(R.drawable.android_circle);
     }
 }
