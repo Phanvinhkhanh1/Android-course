8b23d78bf86ff80824c67c5d1ba59e0789d0a9b8
Phan Vinh Khanh <Khanh161297@gmail.com>
2022-11-01 20:57:39 +0700

    Volley Request in Android

---
+++ 61_VolleyRequest/app/src/main/java/com/example/a61_volleyrequest/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.a61_volleyrequest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String url = "https://www.google.com/";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                Log.d("Error", error.toString());
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+}
